621e3176feafca575f09833c85c303c4bf85c7ae
Gregory Adam Cunningham <gregory.cunningham8276@gmail.com>
2016-03-21 18:47:35 -0400

    clean up and tweaking

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,63 @@
-angular.module("quizApp", [])
-.controller('mainCtrl', function($scope) {
+// var widow = angular.module('widow', [])
+// var tukayyid = anuglar.module('tukayyid', [])
+var app = angular.module('quizApp', [])
+
+app.controller('mainCtrl', function($scope) {
   $scope.widow = function () {
     alert('December 7, 3057');
+  };
+});
+
+app.controller('secondCtrl', function($scope) {
+  $scope.tukayyid = function () {
+    alert('May 1, 3052');
+  };
+});
+
+app.controller('thirdCtrl', function($scope) {
+  $scope.marik = function () {
+    alert('House Marik');
+  };
+});
+
+app.controller('fourthCtrl', function($scope) {
+  $scope.mackie = function () {
+    alert('Mackie');
+  };
+});
+
+app.controller('fifthCtrl', function($scope) {
+  $scope.strana = function () {
+    alert('Strana Mechty');
+  };
+});
+
+app.controller('sixthCtrl', function($scope) {
+  $scope.ian = function () {
+    alert('Ian Davion');
+  };
+});
+
+app.controller('seventhCtrl', function($scope) {
+  $scope.shiro = function () {
+    alert('Shiro Kurita');
+  };
+});
+
+app.controller('eighthCtrl', function($scope) {
+  $scope.hesperus = function () {
+    alert('Hesperus II');
+  };
+});
+
+app.controller('ninthCtrl', function($scope) {
+  $scope.sian = function () {
+    alert('Sian');
+  };
+});
+
+app.controller('tenthCtrl', function($scope) {
+  $scope.star = function () {
+    alert('July 9, 2571');
   };
 });
